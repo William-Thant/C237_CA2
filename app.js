77529613392b23183ddc4fc4b60b181d5886f6d4
William Thant <24024565@myep.edu.sg>
2025-07-28 12:44:38 +0800

    Updated

--- app.js
+++ app.js
@@ -2,23 +2,21 @@ const express = require('express');
 const mysql = require('mysql2');
 
 //******** TODO: Insert code to import 'express-session' *********//
-const session = require('express-session');
+
 
 const flash = require('connect-flash');
 
 const app = express();
 
+const session = require('express-session');
+
 // Database connection
 const db = mysql.createConnection({
-    //host: 'localhost',
-    //user: 'root',
-    //password: 'Republic_C207',
-    //database: 'C237_usersdb'
-    host: 'ohqo4n.h.filess.io',
-    port: 3307,
-    user: 'C237database_frogbattle',
-    password: '929bf98c5cfcac49cde6e6b323d9698aea2797b7',
-    database: 'C237database_frogbattle'
+    host: 'c237-all.mysql.database.azure.com',
+    port: 3306,
+    user: 'c237admin',
+    password: 'c2372025!',
+    database: 'c237_22_team103'
 });
 
 db.connect((err) => {
@@ -33,12 +31,12 @@ app.use(express.static('public'));
 
 //******** TODO: Insert code for Session Middleware below ********//
 app.use(session({
-    secret: 'secret',
+    secret:'secret', //session id to be hashed with this secret to prevent changes of session id
     resave: false,
     saveUninitialized: true,
-    // Session expires after 1 week of inactivity
-    cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}
-}));
+    //session expire after 1 week of inactivity
+    cookie:{ maxAge: 1000*60*60*24*7}
+}))
 
 app.use(flash());
 
@@ -46,29 +44,39 @@ app.use(flash());
 app.set('view engine', 'ejs');
 
 //******** TODO: Create a Middleware to check if user is logged in. ********//
-const checkAuthenticated = (req, res, next) => {
-    if (req.session.user) {
+const checkAuthenticated =(req, res, next) =>{
+    if(req.session.user){
         return next();
-    } else {
-        req.flash('error', 'Please log in to view this resource');
+    } 
+    else {
+        req.flash('error','Please log in to view this resource')
         res.redirect('/login');
     }
-};
-
+}
 //******** TODO: Create a Middleware to check if user is admin. ********//
-const checkAdmin = (req, res, next) => {
-    if (req.session.user.role === 'admin') {
+const checkAdmin =(req, res, next) =>{
+    if(req.session.user.role ==='admin'){
         return next();
-    } else {
-        req.flash('error', 'Access denied');
+    } 
+    else {
+        req.flash('error','Access denie')
         res.redirect('/dashboard');
     }
-};
+}
+
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index', { user: req.session.user, messages: req.flash('success')});
-});
+app.get('/index',(req,res)=>{
+    const sql ="SELECT * FROM products";
+    db.query(sql,(error,results)=>{
+        if(error){
+            console.error("Database query error:",error.message);
+            return res.status(500).send("Error Retrieving users");
+
+        }
+        res.render("index",{products: results});
+    })
+})
 
 app.get('/register', (req, res) => {
     res.render('register', { messages: req.flash('error'), formData: req.flash('formData')[0] });
@@ -76,94 +84,266 @@ app.get('/register', (req, res) => {
 
 
 //******** TODO: Create a middleware function validateRegistration ********//
-const validateRegistration = (req, res, next) => {
-    const { username, email, password, address, contact } = req.body;
+const validateRegistration = (req,res, next) =>{
+    const { username,image,email,password,address,contact,role} =req.body;
 
-    if (!username || !email || !password || !address || !contact) {
+    if (!username || !image || !email || !password || !address || !contact || !role ) {
         return res.status(400).send('All fields are required.');
     }
-    
-    if (password.length < 6) {
-        req.flash('error', 'Password should be at least 6 or more characters long');
+    if (password.length <6){
+        req.flash('error','Password should be at least 6 or more characters long');
         req.flash('formData', req.body);
-        return res.redirect('/register');
+        return res.redirect('/register')
     }
+
     next();
 };
-
 
 //******** TODO: Integrate validateRegistration into the register route. ********//
 app.post('/register', validateRegistration, (req, res) => {
     //******** TODO: Update register route to include role. ********//
-    const { username, email, password, address, contact, role} = req.body;
+    const { username,image, email, password, address, contact ,role} = req.body;
 
-    const sql = 'INSERT INTO users (username, email, password, address, contact, role) VALUES (?, ?, SHA1(?), ?, ?, ?)';
-    db.query(sql, [username, email, password, address, contact, role], (err, result) => {
+    const sql = 'INSERT INTO users (username, image, email, password, address, contact, role) VALUES (?, ?, ?, SHA1(?), ?, ?, ?)';
+    db.query(sql, [username,image, email, password, address, contact, role], (err, result) => {
         if (err) {
             throw err;
         }
         console.log(result);
         req.flash('success', 'Registration successful! Please log in.');
-        res.redirect('/login');
+        res.redirect('/dashboard');
     });
 });
+
+
+
+app.get('/addproduct', (req, res)=>{
+    res.render('addproduct');
+});
+app.post('/addproduct', (req, res)=>{
+    //Extract product date from the request body
+    const {product_name, product_date, image, price, size} = req.body;
+    const sql = 'INSERT INTO products (product_name, product_date, image, price, size) VALUES (?,?,?,?,?)';
+    db.query( sql, [product_name, product_date, image, price, size],(error,result)=>{
+        if (error) {
+            console.error("Error adding product:", error);
+            res.status(500).send("Error adding product");
+
+        } else{
+            res.redirect('/index');
+        }
+    })
+})
 
 //******** TODO: Insert code for login routes to render login page below ********//
-app.get('/login', (req, res) => {
-    res.render('login', { 
-        messages: req.flash('success'), 
-        errors: req.flash('error') 
-    });
-});
+app.get('/',(req,res)=>{
+    res.render('login',{
+        messages: req.flash('success'),
+        errors: req.flash('error')
+    })
+})
 
 //******** TODO: Insert code for login routes for form submission below ********//
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
+app.post('/login',(req,res) =>{
+    const {email,password}=req.body;
 
-    // Validate email and password
-    if (!email || !password) {
-        req.flash('error', 'All fields are required.');
-        return res.redirect('/login');
+    //validate email and password
+    if (!email || !password){
+        req.flash('error', 'All fields are required');
+        return res.redirect('/index')
     }
 
-    const sql = 'SELECT * FROM users WHERE email = ? AND password = SHA1(?)';
-    db.query(sql, [email, password], (err, results) => {
-        if (err) {
+    const sql ='SELECT * FROM users WHERE email =? AND password = SHA1(?)';
+    db.query(sql,[email,password],(err,result)=>{
+        if(err) {
             throw err;
         }
 
-        if (results.length > 0) {
-            // Successful login
-            req.session.user = results[0]; // store user in session
-            req.flash('success', 'Login successful!');
-            //******** TO DO: Update to redirect users to /dashboard route upon successful log in ********//
-            res.redirect('/dashboard');
+        if (result.length > 0) {
+            //successful login
+            req.session.user = result[0];
+            req.flash('success','Login successful!');
+            if (result[0].role === 'admin') {
+                res.redirect('/dashboard')
+            }
+            else if (result[0].role === 'user') {
+                res.redirect('/index')
+            }
+
+        }
+        else{
+            //invalid credentials
+            req.flash('error','Invalid  email or password.');
+            res.redirect('/');
+
+        }
+    })
+})
+//******** TODO: Insert code for dashboard route to render dashboard page for users. ********//
+app.get('/dashboard', checkAuthenticated, checkAdmin,(req, res) => {
+    const sql = 'SELECT * FROM users';
+
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error("Database error:", err);
+            res.status(500).send('Error fetching users');
         } else {
-            // Invalid credentials
-            req.flash('error', 'Invalid email or password.');
-            res.redirect('/login');
+            res.render('dashboard', { users: result }); // ✅ Pass users to dashboard.ejs
         }
     });
 });
-
-
-//******** TODO: Insert code for dashboard route to render dashboard page for users. ********//
-app.get('/dashboard', checkAuthenticated, (req, res) => {
-    res.render('dashboard', { user: req.session.user });
-});
-
 //******** TODO: Insert code for admin route to render dashboard page for admin. ********//
-app.get('/admin', checkAuthenticated, checkAdmin, (req, res) => {
-    res.render('admin', { user: req.session.user });
+app.get('/productdashboard', checkAuthenticated, checkAdmin, (req, res) => {
+    db.query('SELECT * FROM products', (err, products) => {
+        if (err) {
+            console.error("Database error:", err);
+            return res.status(500).send('Error fetching products');
+        }
+        res.render('productdashboard', { user: req.session.user, products: products });
+    });
 });
-
 //******** TODO: Insert code for logout route ********//
-app.get('/logout', (req, res) => {
+app.get('/logout',(req,res)=>{
     req.session.destroy();
     res.redirect('/');
+})
+
+app.get('/deleteuser/:id',(req,res)=>{
+    const userid = req.params.id;
+    const sql ="DELETE FROM users WHERE id = ?";
+    db.query(sql, [userid],(error,results)=>{
+        if(error){
+            console.error("Error deleting users", error)
+            res.status(500).send('Error deleting users')
+        }
+        else{
+            res.redirect('/dashboard');
+        }
+    })
+})
+
+app.get('/deleteproduct/:productID',(req,res)=>{
+    const productID = req.params.productID;
+    const sql ="DELETE FROM products WHERE productID = ?";
+    db.query(sql, [productID],(error,results)=>{
+        if(error){
+            console.error("Error deleting products", error)
+            res.status(500).send('Error deleting products')
+        }
+        else{
+            res.redirect('/index');
+        }
+    })
+})
+
+app.get('/edituser/:id',(req,res)=>{
+    const userID=req.params.id;
+    const sql ="SELECT * FROM users WHERE id =?";
+    db.query( sql,[userID],(error,results)=>{
+        if (error) {
+            console.error("Database querry error:", error.message);
+            return res.status(500).send('Error retrieving user by ID');
+
+        }
+        if (results.length >0){
+            res.render('edituser',{user: results[0]});
+
+        }
+        else{
+            res.status(404).send("user not found");
+        }
+    })
+})
+
+app.post('/edituser/:id',(req,res)=>{
+    const userID = req.params.id;
+    const {username, address, contact, password, email, image, role} = req.body;
+    const sql ="UPDATE users SET username = ?,address = ?, contact = ?, password = SHA1(?), email =?, image = ? ,role =? WHERE id=?";
+    db.query( sql, [username,address,contact,password,email,image,role,userID], (error,results)=>{
+        if (error) {
+            console.error("Error updating user:", error);
+            res.status(500).send("Error updating user");
+
+        }
+        else {
+            res.redirect('/dashboard')
+        }
+    })
 });
 
-// Starting the server
+app.get('/adminregister', (req, res) => {
+    res.render('adminregister', { messages: req.flash('error'), formData: req.flash('formData')[0] });
+});
+
+
+//******** TODO: Create a middleware function validateRegistration ********//
+const validateAdminRegistration = (req,res, next) =>{
+    const { username,image,email,password,address,contact,role} =req.body;
+
+    if (!username || !image || !email || !password || !address || !contact || !role ) {
+        return res.status(400).send('All fields are required.');
+    }
+    if (password.length <6){
+        req.flash('error','Password should be at least 6 or more characters long');
+        req.flash('formData', req.body);
+        return res.redirect('/adminregister')
+    }
+
+    next();
+};
+
+//******** TODO: Integrate validateRegistration into the register route. ********//
+app.post('/adminregister', validateAdminRegistration, (req, res) => {
+    //******** TODO: Update register route to include role. ********//
+    const { username,image, email, password, address, contact ,role} = req.body;
+
+    const sql = 'INSERT INTO users (username, image, email, password, address, contact, role) VALUES (?, ?, ?, SHA1(?), ?, ?, ?)';
+    db.query(sql, [username,image, email, password, address, contact, role], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+        req.flash('success', 'Registration successful! Please log in.');
+        res.redirect('/dashboard');
+    });
+});
+
+app.get('/editproduct/:productID',(req,res)=>{
+    const productID =req.params.productID;
+    const sql ="SELECT * FROM products WHERE productID =?";
+    db.query( sql,[productID],(error,results)=>{
+        if (error) {
+            console.error("Database querry error:", error.message);
+            return res.status(500).send('Error retrieving user by ID');
+
+        }
+        if (results.length >0){
+            res.render('editproduct',{product: results[0]});
+
+        }
+        else{
+            res.status(404).send("Product not found");
+        }
+    })
+})
+
+app.post('/editproduct/:id',(req,res)=>{
+    const productID = req.params.id;
+    const {product_name, product_date, price, size, image} = req.body;
+    const sql ="UPDATE products SET product_name = ?,product_date = ?, price = ?, size = ? , image = ? WHERE productID = ?";
+    db.query( sql, [product_name, product_date, price, size,image, productID], (error,results)=>{
+        if (error) {
+            console.error("Error updating product:", error);
+            res.status(500).send("Error updating product");
+
+        }
+        else {
+            res.redirect('/productdashboard')
+        }
+    })
+});
+
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
